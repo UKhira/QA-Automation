e54e3b9caaaf107d8c47b8c72e16d65b3bd889a8
Udith Kavishka <udithmanohara@gmail.com>
2023-04-18 23:34:59 +0530

    Update team35QA.java

--- src/main/java/team35QA.java
+++ src/main/java/team35QA.java
@@ -6,15 +6,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
 public class team35QA {
     WebDriver driver;
